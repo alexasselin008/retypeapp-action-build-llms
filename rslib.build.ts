0785dce44a0f7f178911d674d90ffd65349e8f66
Alexandre Asselin <alexasselin008@gmail.com>
2025-08-16 00:29:35 -0400

    update lib

--- rslib.build.ts
+++ rslib.build.ts
@@ -1,5 +1,8 @@
 import { defineBuildConfig } from "@workleap/rslib-configs";
+import builtinModulesList from "builtin-modules";
 import path from "node:path";
+
+const builtinModules = new Set(builtinModulesList);
 
 export default defineBuildConfig({
     bundle: true,
@@ -9,5 +12,25 @@ export default defineBuildConfig({
     tsconfigPath: path.resolve("./tsconfig.build.json"),
     target: "node",
     dts: false,
-    sourceMap: false
+    sourceMap: false,
+    transformers: [
+        config => {
+            config.output = {
+                ...config.output,
+                externals(data, callback) {
+                    callback(undefined, builtinModules.has(data.request!));
+                }
+            };
+
+            config.performance = {
+                ...config.performance,
+                chunkSplit: {
+                    ...config.performance?.chunkSplit,
+                    strategy: "all-in-one"
+                }
+            };
+
+            return config;
+        }
+    ]
 });
